4da7b932e0fe19afbc0f4572f1b5201e81a6b5fb
Layso <canyilmaz1997@gmail.com>
2022-02-09 21:15:56 +0300

    Package name updated from com.freereader to com.laysoftware.freereader

--- android/app/src/main/java/com/laysoftware/freereader/MainActivity.java
+++ android/app/src/main/java/com/laysoftware/freereader/MainActivity.java
@@ -1,4 +1,4 @@
-package com.freereader;
+package com.laysoftware.freereader;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/laysoftware/freereader/MainApplication.java
+++ android/app/src/main/java/com/laysoftware/freereader/MainApplication.java
@@ -1,4 +1,4 @@
-package com.freereader;
+package com.laysoftware.freereader;
 
 import android.app.Application;
 import android.content.Context;
